92ee12c17259d6c051f3f8c5bdb443af40fc97f6
PavolPala <palap@ipoint.sk>
2015-11-18 19:06:23 +0100

    Added testing projects

---
+++ test.plugins/others/FirstPlugin/FirstPlugin.cs
@@ -0,0 +1,50 @@
+﻿using PluginContracts;
+using Events;
+
+namespace FirstPlugin
+{
+    public class FirstPlugin : IPlugin
+	{
+        private IHost _host;
+
+        public FirstPlugin()
+        {
+        }
+
+        #region IPlugin Members
+
+        public string Name
+		{
+			get
+			{
+				return "First Plugin";
+			}
+		}
+
+        public void Init(IHost h)
+        {
+            _host = h;
+            _host.proxy.addEventHandler(EventKind.GenericEvent, HnadleGenericEvent);
+            _host.proxy.addEventHandler(EventKind.CloseEvent, HandleCloseEvent);
+
+        }
+
+        public void Do()
+		{
+            System.Console.WriteLine("Do Something in First Plugin");
+            _host.proxy.fireEvent(new GenericEvent(this, "message form first"));
+        }
+
+        #endregion
+
+        private void HnadleGenericEvent(EventData e)
+        {
+               System.Console.WriteLine("First plugin : Som tu !!! : " + e.Data);
+        }
+
+        private void HandleCloseEvent(EventData e)
+        {
+            System.Console.WriteLine("First plugin : Close !!! : " + e.Data);
+        }
+    }
+}
---
+++ test.plugins/others/SecondPlugin/SecondPlugin.cs
@@ -0,0 +1,50 @@
+﻿using PluginContracts;
+using Events;
+
+namespace SecondPlugin
+{
+	public class SecondPlugin : IPlugin
+	{
+        private IHost _host;
+
+        public SecondPlugin()
+        {
+        }
+
+        #region IPlugin Members
+
+        public string Name
+		{
+			get
+			{
+				return "Second Plugin";
+			}
+		}
+
+        public void Init(IHost h)
+        {
+            _host = h;
+            _host.proxy.addEventHandler(EventKind.GenericEvent, HnadleGenericEvent);
+            _host.proxy.addEventHandler(EventKind.CloseEvent, HandleCloseEvent);
+
+        }
+
+        public void Do()
+		{
+            System.Console.WriteLine("Do Something in Second Plugin");
+            _host.proxy.fireEvent(new GenericEvent(this, "message form second"));
+        }
+
+        #endregion
+
+        private void HnadleGenericEvent(EventData e)
+        {
+            System.Console.WriteLine("Second plugin : Som tu !!! : " + e.Data);
+        }
+
+        private void HandleCloseEvent(EventData e)
+        {
+            System.Console.WriteLine("Second plugin : Close !!! : " + e.Data);
+        }
+    }
+}
